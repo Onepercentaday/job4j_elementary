309d07c341a3c5325e2724ee2681582bea0bd0b3
Onepercentaday <cripto.trader@mail.ru>
2022-11-15 22:35:25 +0300

    2. Тесты для задачи "идеальный вес" [#159458]

---
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FitTest {
+
+    @Test
+    public void whenManWeight190Then103499() {
+        short in = 190;
+        double expected = 103.499;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+
+    }
+
+    @Test
+    public void whenWomanWeight150Then46() {
+        short in = 150;
+        double expected = 46;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
