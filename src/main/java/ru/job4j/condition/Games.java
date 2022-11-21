cfdd26d59bad348389b0d1342e3ec61ec0371b6b
Onepercentaday <cripto.trader@mail.ru>
2022-11-21 21:52:04 +0300

    4.3.1. Сокращенный оператор && [#333783]

---
+++ src/main/java/ru/job4j/condition/Games.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Games {
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        if (allowByParent && hasMoney) {
+            System.out.println("I can go in the computer club");
+        } else {
+            System.out.println("I can not");
+        }
+    }
+
+    public static void main(String[] args) {
+        Games.permission(true, true);
+        Games.permission(true, false);
+        Games.permission(false, true);
+        Games.permission(false, false);
+    }
+}
