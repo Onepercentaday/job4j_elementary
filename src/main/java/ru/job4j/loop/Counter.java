cc1fdcf582cd13c61d6eae173f738ac9e7980850
Onepercentaday <cripto.trader@mail.ru>
2022-11-24 21:51:01 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -9,10 +9,24 @@ public class Counter {
         return sum;
     }
 
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         System.out.println(sum(0, 5));
         System.out.println(sum(3, 7));
         System.out.println(sum(4, 12));
         System.out.println(sum(0, 100));
+
+        System.out.println(sumByEven(0, 6));
+        System.out.println(sumByEven(3, 19));
+        System.out.println(sumByEven(1, 1));
     }
 }
