33babede3491db59f63dd349a4921c6ba8e1c8dd
Onepercentaday <cripto.trader@mail.ru>
2022-11-29 21:17:10 +0300

    5.4. Протеиновая диета (цикл while) [#156313 #365411]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -4,14 +4,10 @@ public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
         while (ivan <= nik) {
-            month = month + 1;
+            month++;
             ivan *= 3;
             nik *= 2;
         }
         return month;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(Fitness.calc(90, 90));
     }
 }
