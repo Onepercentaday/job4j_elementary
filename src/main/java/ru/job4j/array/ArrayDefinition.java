e2452d038c233ca6e8a2f8bad31dd62622d70a66
Onepercentaday <cripto.trader@mail.ru>
2022-11-30 22:22:31 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива surnames равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива prises равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Анна";
+        names[1] = "Михаил";
+        names[2] = "Алксандр";
+        names[3] = "Владимир";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
