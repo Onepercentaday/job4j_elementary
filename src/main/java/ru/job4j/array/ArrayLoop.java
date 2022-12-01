588478400f649eb6cbea6a6f368b1cb8764e75af
Onepercentaday <cripto.trader@mail.ru>
2022-12-01 23:15:32 +0300

    6.0.5. Массивы и цикл for-each [#333581 #366594]

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -6,7 +6,7 @@ public class ArrayLoop {
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = index * 2 + 3;
         }
-        for (int num: numbers) {
+        for (int num : numbers) {
             System.out.println(num);
         }
     }
